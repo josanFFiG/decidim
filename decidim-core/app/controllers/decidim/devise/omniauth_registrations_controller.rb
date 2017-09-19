1d31779b5491f096d261b48a113996bbd6df16ba
David Rodríguez <deivid.rodriguez@riseup.net>
2017-09-20 00:50:15 +0200

    Stop authorizing right after first login
    Decidim is very usable without authorizations. We should not bother
    users right after registration until they do something that requires
    authorization. Otherwise we can let them enjoy all the decidim goodness
    that don't require authorizations.
    
    If apps wanted to override this, I think app they are still able to do
    it by overriding the sessions controller, and they won't have to bother
    with the number of enabled authorizations and so on like it happens when
    we provide it in core.

--- decidim-core/app/controllers/decidim/devise/omniauth_registrations_controller.rb
+++ decidim-core/app/controllers/decidim/devise/omniauth_registrations_controller.rb
@@ -43,26 +43,6 @@ module Decidim
         end
       end
 
-      def after_sign_in_path_for(user)
-        if !pending_redirect?(user) && first_login_and_not_authorized?(user)
-          authorizations_path
-        else
-          super
-        end
-      end
-
-      # Calling the `stored_location_for` method removes the key, so in order
-      # to check if there's any pending redirect after login I need to call
-      # this method and use the value to set a pending redirect. This is the
-      # only way to do this without checking the session directly.
-      def pending_redirect?(user)
-        store_location_for(user, stored_location_for(user))
-      end
-
-      def first_login_and_not_authorized?(user)
-        user.is_a?(User) && user.sign_in_count == 1 && Decidim.authorization_handlers.any?
-      end
-
       def action_missing(action_name)
         return send(:create) if devise_mapping.omniauthable? && User.omniauth_providers.include?(action_name.to_sym)
         raise AbstractController::ActionNotFound, "The action '#{action_name}' could not be found for Decidim::Devise::OmniauthCallbacksController"
--- decidim-core/app/controllers/decidim/devise/sessions_controller.rb
+++ decidim-core/app/controllers/decidim/devise/sessions_controller.rb
@@ -6,24 +6,6 @@ module Decidim
     class SessionsController < ::Devise::SessionsController
       include Decidim::DeviseControllers
 
-      def after_sign_in_path_for(user)
-        return first_login_authorizations_path if first_login_and_not_authorized?(user) &&
-                                                  !user.admin?
-        super
-      end
-
-      # Calling the `stored_location_for` method removes the key, so in order
-      # to check if there's any pending redirect after login I need to call
-      # this method and use the value to set a pending redirect. This is the
-      # only way to do this without checking the session directly.
-      def pending_redirect?(user)
-        store_location_for(user, stored_location_for(user))
-      end
-
-      def first_login_and_not_authorized?(user)
-        user.is_a?(User) && user.sign_in_count == 1 && current_organization.available_authorizations.any?
-      end
-
       def after_sign_out_path_for(user)
         request.referer || super
       end
