4309f481252699b2621118f0ca0463dfff12572b
David Rodríguez <deivid.rodriguez@riseup.net>
2017-09-24 18:42:09 +0200

    Extract a shared example

--- spec/generator_spec.rb
+++ spec/generator_spec.rb
@@ -5,43 +5,39 @@ describe "Application generation" do
 
   after { FileUtils.rm_rf("tmp/test_app") }
 
-  context "without flags" do
-    let(:command) { "bin/decidim tmp/test_app" }
-
+  shared_examples_for "a sane generator" do
     it "successfully generates application" do
       expect(status).to eq(true)
     end
+  end
+
+  context "without flags" do
+    let(:command) { "bin/decidim tmp/test_app" }
+
+    it_behaves_like "a sane generator"
   end
 
   context "with --edge flag" do
     let(:command) { "bin/decidim --edge tmp/test_app" }
 
-    it "successfully generates application" do
-      expect(status).to eq(true)
-    end
+    it_behaves_like "a sane generator"
   end
 
   context "with --branch flag" do
     let(:command) { "bin/decidim --branch master tmp/test_app" }
 
-    it "successfully generates application" do
-      expect(status).to eq(true)
-    end
+    it_behaves_like "a sane generator"
   end
 
   context "with --path flag" do
     let(:command) { "bin/decidim --path #{File.expand_path("..", __dir__)} tmp/test_app" }
 
-    it "successfully generates application" do
-      expect(status).to eq(true)
-    end
+    it_behaves_like "a sane generator"
   end
 
   context "development application" do
     let(:command) { "bundle exec rake development_app" }
 
-    it "successfully generates application" do
-      expect(status).to eq(true)
-    end
+    it_behaves_like "a sane generator"
   end
 end
